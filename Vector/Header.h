6a6ee583c8cdb0b8baba67c73bcce2c40a5e54df
Vlad <vlad.miryaha@gmail.com>
2017-03-02 14:54:46 +0300

    fix it
    read comments

--- Vector/Header.h
+++ Vector/Header.h
@@ -8,6 +8,7 @@
 struct Vector2
 {
 private:
+	/* Как поля менять будете? Лучше в публик вынести в данном случае */
 	double x;
 	double y;
 
@@ -19,6 +20,17 @@ public:
 	int   Vector2::vector2DMP();
 	bool  Vector2::vector2OK();
 
+	/*
+	FIXIT:
+		Vector2 operator +  (const Vector2& other) const;
+		Vector2& operator += (const Vector2& other);
+		
+		Vector2& rotate      (      double   angle    );
+		Vector2 getRotated  (      double   angle    );
+		
+		Vector2 norm() const;
+	        Vector2 perp() const;
+	*/
 	const Vector2 operator +  (const Vector2& other);
 	const Vector2 operator -  (const Vector2& other);
 	      Vector2 operator += (const Vector2& other);
