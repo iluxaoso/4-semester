dbbf7edf4081dcc2959d009425aafa0833ff8546
iluxaoso <ilyaosovskiy@mail.ru>
2017-03-20 19:28:30 +0300

    Add files via upload

---
+++ PI_Definer/Checker.cpp
@@ -0,0 +1,10 @@
+#include "include.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << M_PI << "\n";
+	system("pause");
+
+	return 0;
+}
---
+++ PI_Definer/include.h
@@ -0,0 +1,4 @@
+#pragma once
+#define _USE_MATH_DEFINES //!Писать неодходимо ПЕРЕД всеми include, если используется <cmath>; 
+						  //!Писать можно ПОСЛЕ всех include, если используется <math.h>
+#include <cmath>
