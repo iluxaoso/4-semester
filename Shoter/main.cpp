2c6d516e3f36f1f2c53a7eab492348524fe24e45
iluxaoso <ilyaosovskiy@mail.ru>
2017-03-22 23:26:57 +0300

    Update main.cpp

--- Shoter/main.cpp
+++ Shoter/main.cpp
@@ -1,6 +1,6 @@
 #include "Shooter.h"
 #include "includes.h"
-
+//Не знаю почему, но программа не работает. Я ее несколько раз переделывал, но все равно не получается
 int main()
 {
 	Shooter shooter;
